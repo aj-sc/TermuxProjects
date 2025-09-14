305c4f91ce54a547c0e0ce2a77207a2d96f808cf
aj-sc <alberth0804@gmail.com>
2025-09-14 14:52:15 -0500

    note adding fixed, json list set for creation of notes with id autoincrement

--- notes_cli/main.py
+++ notes_cli/main.py
@@ -1,18 +1,29 @@
 import json
-import os
 
 title = input('Write the title: ')
 body = input('Write the body: ')
 
+def get_notes():
+    with open('notes_db.json', 'r') as file:
+        data = json.load(file)
+
+    return data
+
 def add_note(note_title, note_body):
+    notes_data = get_notes()
+
+    note_id = notes_data[-1].get('id', '') + 1 if len(notes_data) > 0 else 1
+
     note = {
+        'id' : note_id,
         'title' : note_title,
         'body' : note_body
         }
 
-    if not os.path.isfile('notes_db.json'):
-        with open('notes_db.json', 'w') as file:
-            json.dump(note, file, indent=4)
-            print('db created')
+    notes_data.append(note)
+
+    with open('notes_db.json', 'w') as file:
+        json.dump(notes_data, file, indent=4)
+        print('Note added to db')
 
 add_note(title, body)
