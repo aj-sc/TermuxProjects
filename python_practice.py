4b41e0ecb2457532c291805f6ac99b3638b5b04d
aj-sc <alberth0804@gmail.com>
2025-09-14 10:23:33 -0500

    added proper json indentation

--- python_practice.py
+++ python_practice.py
@@ -1,9 +1,13 @@
-note_title = input('Write the title')
-note_body = input('Write the body')
+import json
+
+note_title = input('Write the title: ')
+note_body = input('Write the body: ')
 
 note = {
     'title' : note_title,
     'body' : note_body
     }
 
-print(note)
+data = json.dumps(note, indent=4)
+
+print(data)
