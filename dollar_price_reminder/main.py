dfe113afaf6bf9e70c51dcc2f9f1405a5cdcc790
Albert Salas Cobilla <alberth0804@gmail.com>
2025-09-15 14:20:36 -0500

    Removed Socrata API and rebuild code using requests library

--- dollar_price_reminder/main.py
+++ dollar_price_reminder/main.py
@@ -1,5 +1,41 @@
-from sodapy import Socrata
+import requests
+from datetime import date, datetime
 
-client = Socrata('www.datos.gov.co', None)
+BASE_URL = 'https://www.datos.gov.co/resource/ceyp-9c7c.json'
 
-results = client.get("ceyp-9c7c", limit=2000)
+def set_date_limit() -> str:
+    date_limit = datetime.now()
+    formatted_date_limit = date_limit.strftime('%Y-%m-%dT00:00:00.000')
+
+    return formatted_date_limit
+
+def get_dollar_value(date_limit: str) -> float | str:
+    params = {
+        'vigenciahasta' : date_limit
+    }
+
+    response = requests.get(BASE_URL, params=params)
+    response.raise_for_status()
+    data = response.json()
+
+    if not data:
+        return 'No data returned from API'
+    
+    dollar_value = float(data[0].get('valor', ''))
+
+    return dollar_value
+
+def main() -> None:
+    date_limit = set_date_limit()
+    dollar_value = get_dollar_value(date_limit)
+
+    if isinstance(dollar_value, float):
+        print(f'Dollar value on {date.today()} is: ${dollar_value:,.2f}')
+    else:
+        print(dollar_value)
+
+if __name__ == "__main__":
+    main()
+
+    
+
